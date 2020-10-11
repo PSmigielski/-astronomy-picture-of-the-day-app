de592e9e35b050c309293c3bbff09af65fe283d0
PSmigielski <walek.smigielski@gmail.com>
2020-10-11 21:28:02 +0200

    add styling to home components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import './App.css';
+import './style/App.css';
+//import components
+import Logo from './components/Logo';
+import BlobLeft from './components/BlobLeft';
+import BlobRight from './components/BlobRight';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-
+      <Logo />
+      <BlobLeft /> 
+      <BlobRight /> 
+      <Hero/>
     </div>
   );
 }
-
 export default App;
