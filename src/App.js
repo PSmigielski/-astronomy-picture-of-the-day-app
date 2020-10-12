1518dbd8e929730414e0a0fe2d498c7af77f770d
PSmigielski <walek.smigielski@gmail.com>
2020-10-12 11:34:37 +0200

    add photoview to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {useState, useEffect} from 'react';
 // import views
-import Home from './views/Home'
+import Home from './views/Home';
+import PhotoView from './views/PhotoView';
 function App() {
   const [data, setData] = useState({})
   useEffect(()=>{
-    if(data.headers) console.log(data);
+    if(data.date) console.log(data);
   }, [data])
   return (
     <div className="App">
       <Home setData={setData}/>
+      <PhotoView data={data}/>
     </div>
   );
 }
