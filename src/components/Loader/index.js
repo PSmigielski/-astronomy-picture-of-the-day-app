3cd1cc4e1f7e7dd38ff1bb1542a4571c44d6288e
PSmigielski <walek.smigielski@gmail.com>
2020-10-14 11:28:09 +0200

    add loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Loader = () => {
+  return(
+    <div className="loaderWrapper">
+      <div className="circle1">
+          <div className="circle2"></div>
+      </div>
+  </div>
+  )
+}
+
+export default Loader;
