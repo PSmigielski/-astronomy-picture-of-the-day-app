ba3f4621620ee2c6603e5375b87417171b85fc8a
PSmigielski <walek.smigielski@gmail.com>
2020-10-14 11:29:44 +0200

    add loader to photo view

--- src/views/PhotoView.js
+++ src/views/PhotoView.js
@@ -4,21 +4,22 @@ import ResponsiveEmbed from 'react-responsive-embed';
 import Arrow from '../components/Arrow';
 import DataContainer from '../components/DataContainer';
 import Logo from '../components/Logo';
+import Loader from '../components/Loader';
 import '../style/main.css';
 
-const PhotoView = ({ data, setRecived }) => {
+const PhotoView = ({ data, setRecived, recived }) => {
   return(
     <div className="photoView">
       <Arrow setRecived={setRecived}/>
       <Logo preview="true"/>
-      <div className="dataWrapper">
+{recived ? <div className="dataWrapper">
         <div className="pictureContaier">
           {data.media_type==="video" ? 
             <ResponsiveEmbed src={data.url} allowFullScreen/>
           : <img className="dataImage" src={data.url} alt={data.title ? data.title : 'alternative'}/ >}
         </div>
         <DataContainer  data={ data }/>
-      </div>
+      </div> : <Loader />}
     </div>
   )   
 }
